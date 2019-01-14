51502a5e2e2f2dbc8b4e670711bce013a940cf8d
cohen52 <31905089+cohen52@users.noreply.github.com>
2019-01-14 18:25:46 -0500

    Initial Commit

---
+++ flixster/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  flixster
+//
+//  Created by Max Cohen on 1/14/19.
+//  Copyright © 2019 Max Cohen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
